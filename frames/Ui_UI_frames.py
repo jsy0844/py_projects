b5538bc1b8623bbf0c09e56f5b4b98dc385afa85
JiangShengyi <jsy0844@163com>
2018-04-26 12:55:09 +0800

    Revert "更新按钮边框"
    This reverts commit 1dc4a0aa8966edc248446d031bbaa597e558e146.

--- frames/Ui_UI_frames.py
+++ frames/Ui_UI_frames.py
@@ -22,43 +22,10 @@ class Ui_MainWindow(object):
         MainWindow.setWindowIcon(icon)
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        self.fileText = QtWidgets.QTextBrowser(self.centralWidget)
-        self.fileText.setGeometry(QtCore.QRect(80, 60, 421, 31))
+
         font = QtGui.QFont()
         font.setFamily("Arial")
-        font.setPointSize(7)
-        self.fileText.setFont(font)
-        self.fileText.setObjectName("fileText")
-        self.LoadBtn = QtWidgets.QPushButton(self.centralWidget)
-        self.LoadBtn.setGeometry(QtCore.QRect(510, 60, 31, 31))
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setBold(False)
-        font.setWeight(50)
-        self.LoadBtn.setFont(font)
-        self.LoadBtn.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
-        self.LoadBtn.setAutoFillBackground(True)
-        self.LoadBtn.setText("")
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("foldericon.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.LoadBtn.setIcon(icon1)
-        self.LoadBtn.setIconSize(QtCore.QSize(40, 40))
-        self.LoadBtn.setObjectName("LoadBtn")
-        self.staBtn = QtWidgets.QPushButton(self.centralWidget)
-        self.staBtn.setGeometry(QtCore.QRect(200, 230, 91, 40))
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-=======
-=======
->>>>>>> parent of 1dc4a0a... 更新按钮边框
-=======
->>>>>>> parent of 1dc4a0a... 更新按钮边框
-=======
->>>>>>> parent of 1dc4a0a... 更新按钮边框
+
         self.fileText = QTextBrowser(self.centralWidget)
         self.fileText.setGeometry(80, 100, 421, 28)
 
@@ -83,50 +50,38 @@ class Ui_MainWindow(object):
         # 设置开始按钮
         self.staBtn = QPushButton(self.centralWidget)
         self.staBtn.setGeometry(200, 200, 81, 40)
->>>>>>> parent of 1dc4a0a... 更新按钮边框
         font.setPointSize(10)
-        font.setBold(False)
-        font.setWeight(50)
         self.staBtn.setFont(font)
         self.staBtn.setObjectName("staBtn")
+
+        # 设置打开图片按钮
         self.colBtn = QtWidgets.QPushButton(self.centralWidget)
         self.colBtn.setGeometry(QtCore.QRect(320, 230, 101, 40))
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(10)
-        font.setBold(False)
-        font.setWeight(50)
         self.colBtn.setFont(font)
         self.colBtn.setObjectName("colBtn")
+
+        # 勾选设置图片大小选项
         self.RechBox = QtWidgets.QCheckBox(self.centralWidget)
-        self.RechBox.setGeometry(QtCore.QRect(210, 120, 171, 27))
-        font = QtGui.QFont()
+        self.RechBox.setGeometry(QtCore.QRect(210, 120, 171, 27))   
         font.setPointSize(9)
-        font.setBold(False)
-        font.setWeight(50)
         self.RechBox.setFont(font)
         self.RechBox.setObjectName("RechBox")
+
+        #设置图片宽高
         self.wetLine = QtWidgets.QLineEdit(self.centralWidget)
         self.wetLine.setGeometry(QtCore.QRect(200, 170, 91, 29))
-        font = QtGui.QFont()
-        font.setBold(False)
-        font.setWeight(50)
         self.wetLine.setFont(font)
         self.wetLine.setObjectName("wetLine")
         self.hetLine = QtWidgets.QLineEdit(self.centralWidget)
         self.hetLine.setGeometry(QtCore.QRect(320, 170, 91, 29))
-        font = QtGui.QFont()
-        font.setBold(False)
-        font.setWeight(50)
         self.hetLine.setFont(font)
         self.hetLine.setObjectName("hetLine")
+        # 乘号
         self.mulLal = QtWidgets.QLabel(self.centralWidget)
         self.mulLal.setGeometry(QtCore.QRect(300, 170, 16, 23))
-        font = QtGui.QFont()
-        font.setBold(False)
-        font.setWeight(50)
         self.mulLal.setFont(font)
         self.mulLal.setObjectName("mulLal")
+        
         MainWindow.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(MainWindow)
